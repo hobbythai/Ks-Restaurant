e038fb0e23dd0bdda35df0fb27e9ecfe21a3b78b
kally <kally.thailand@gmail.com>
2016-04-09 10:08:31 +0700

    start

---
+++ app/src/main/java/com/hobbythai/android/ksrestaurant/MainActivity.java
@@ -0,0 +1,22 @@
+package com.hobbythai.android.ksrestaurant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //explicit
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }//main method
+
+
+
+}//main class

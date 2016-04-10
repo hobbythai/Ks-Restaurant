b57fb475dc38d99f17d8953ef217298dd8268111
kally <kally.thailand@gmail.com>
2016-04-10 14:52:38 +0700

    finish

--- app/src/main/java/com/hobbythai/android/ksrestaurant/ServiceActivity.java
+++ app/src/main/java/com/hobbythai/android/ksrestaurant/ServiceActivity.java
@@ -1,7 +1,10 @@
 package com.hobbythai.android.ksrestaurant;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -10,13 +13,22 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class ServiceActivity extends AppCompatActivity {
 
@@ -101,6 +113,14 @@ public class ServiceActivity extends AppCompatActivity {
                         foodStrings, priceStrings, iconStrings);
                 listView.setAdapter(myAdapter);
 
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                        chooseAmount(foodStrings[position]);
+
+                    }
+                });
 
             } catch (Exception e) {
                 Log.d("10Aprill", "Error strJSON --> " + e.toString());
@@ -109,6 +129,58 @@ public class ServiceActivity extends AppCompatActivity {
         }
 
     }//my connected food
+
+    private void chooseAmount(String choosefoodString) {
+
+        foodString = choosefoodString;
+
+        CharSequence[] charSequence = {"1 จาน", "2 จาน", "3 จาน", "4 จาน", "5 จาน"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.drawable.icon_myaccount);
+        builder.setTitle(foodString);
+        builder.setSingleChoiceItems(charSequence, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                amountString = Integer.toString(i + 1);
+                dialog.dismiss();
+
+                Toast.makeText(getApplicationContext(),"สั่ง "+amountString+" จาน",Toast.LENGTH_LONG).show();
+
+                updateToServer();
+            }
+        });
+        builder.show();
+
+    }//choose
+
+    private void updateToServer() {
+
+        //connect to Server
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        try {
+
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("isAdd", "true"));
+            nameValuePairs.add(new BasicNameValuePair("Officer", officerString));
+            nameValuePairs.add(new BasicNameValuePair("Desk", deskString));
+            nameValuePairs.add(new BasicNameValuePair("Food", foodString));
+            nameValuePairs.add(new BasicNameValuePair("Amount", amountString));
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost("http://swiftcodingthai.com/9Apr/php_add_data_restaurant.php");
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+            httpClient.execute(httpPost);
+
+            Toast.makeText(this,"Update Order to Server",Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(this,"Can't Connent Server",Toast.LENGTH_LONG).show();
+        }
+
+    }
 
 
     private void createSpinner() {
